2cfa10552db937597130de978e8b22b290fd521b
Shravani Jagtap <shravani.jagtap05@gmail.com>
2026-07-17 17:40:49 +0530

    add MongoDB Atlas fallback, update env config

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ const contentRouter = require("./routes/content.js");
 const announcementRouter = require("./routes/announcement.js");
 const flashcardRouter = require("./routes/flashcard.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/tuitionhub";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/tuitionhub";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/tuitionhub";
 
 main()
     .then(() => {
