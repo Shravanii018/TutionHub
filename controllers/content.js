89cb819ca552eb67cd0273105dfec104318c5042
Shravani Jagtap <shravani.jagtap05@gmail.com>
2026-07-18 10:58:52 +0530

    fix cloudinary storage config 2

--- controllers/content.js
+++ controllers/content.js
@@ -1,11 +1,11 @@
 const Note = require("../models/note.js");
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 // show all uploaded notes and PDFs
 module.exports.index = async (req, res) => {
