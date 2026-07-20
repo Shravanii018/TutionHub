d4f85fd7835828743f44e118da11daffa5e4fc8e
Shravani Jagtap <shravani.jagtap05@gmail.com>
2026-07-20 22:51:41 +0530

    Add resource_type auto to Cloudinary upload stream

--- controllers/content.js
+++ controllers/content.js
@@ -41,6 +41,7 @@ module.exports.createContent = async (req, res) => {
         const streamUpload = (req) => {
             return new Promise((resolve, reject) => {
                 const stream = cloudinary.uploader.upload_stream(
+                    { resource_type: "auto" },
                     (error, result) => {
                         if (result) {
                             resolve(result);
